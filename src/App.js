7e287044f0fec4e075f9b5a8ced734d839300143
David Erik Nazareno <daviderikfalsh@yahoo.com.br>
2021-03-12 19:35:31 -0300

    primera pagina

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App(){
+    const projects = [ 'Desenvolvimento de app', 'Front-end web' ];
+
+    return (
+        <>
+        <Header title = "Projects" />
+        <ul>
+            {projects.map(project => <li>{project}</li>)}
+        </ul>
+        </>    
+    );
+}
+
+export default App;
